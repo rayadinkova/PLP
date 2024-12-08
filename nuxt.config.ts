64ae3f28a9b9b1c0916e32363c6c0d32853b5645
Raya Dinkova <rayadinkovadesignss@gmail.com>
2024-12-08 22:56:19 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,12 @@
 export default defineNuxtConfig({
-  // Compatibility date for Nuxt
   compatibilityDate: '2024-11-01',
-
-  // Enable devtools
   devtools: { enabled: true },
-
-  // Include Tailwind CSS
   css: ['~/styles/tailwind.css'],
-
-  // Configure PostCSS for Tailwind and Autoprefixer
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-
-  // Add Pinia module
   modules: ['@pinia/nuxt'],
 });
